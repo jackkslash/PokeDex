d7784a5553a4beb2d8f04b6d6e5db948a5b412cc
mvotho <jackfaulknerbis@hotmail.co.uk>
2022-05-12 23:25:34 +0100

    Images added for each pokemon, Utils file added

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,17 +1,21 @@
+import { GetImageById } from "../util/Utils";
 
-export const PokemonCard = ({name}: {name: string}) => {
+
+export const PokemonCard = ({ props, index }: { props: any, index: string }) => {
+
+  let pokemonId = index +1;
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img
-          src="https://api.lorem.space/image/shoes?w=400&h=225"
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          {name}
-        </h2>
+    <div className="flex flex-row justify-center ">
+      <div
+        tabIndex="0"
+        className="collapse border border-base-300 bg-base-100 rounded-box"
+      >
+        <div className="collapse-title text-xl font-medium">
+          {props.name} {index + 1}/803
+        </div>
+        <div className="collapse-content">
+          <img src={GetImageById(pokemonId)}></img>
+        </div>
       </div>
     </div>
   );
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { PokemonCard } from "../../components/PokemonCard";
 
 export const Home = () => {
-  const { isLoading, isError, data, error } = useQuery("repoData", () =>
+  const { isLoading, isError, data, error } = useQuery("pokemonData", () =>
     axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => res.data)
   );
 
@@ -12,10 +12,10 @@ export const Home = () => {
 
   if (error) return <>"An error has occurred: "</>;
 
-  const mapResults = data.results.map((results: any) => {
+  const mapResults = data.results.map((results: any, index: number) => {
     return (
       <>
-        <PokemonCard name={results.name} />
+        <PokemonCard props={results} index={index}/>
       </>
     );
   });
